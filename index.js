33b04b03a3a9f13c4380c69f34cfdac878055f4b
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-08 12:12:56 -0500

    commit tree 65a4a3d1a9dac0beef4ebceebaab681b1b150a72

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+const finder = document.querySelector('main').remove();
+
+
+const headerEditor = document.createElement("h1",);
+document.body.append(headerEditor);
+
+const newHeader = document.querySelector("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = 'Alex is the champion'
+console.log(newHeader.nodeName);
+console.log(newHeader.id);
+console.log(newHeader.innerHTML);
+
+// console.log(newHeader.id);
+
+// const victoryGetter = document.getElementById("victory");
+// console.log(victoryGetter);
+// const newHeader = document.createElement("h1",);
+// document.body.append(newHeader);
+
+// const headerEditor = document.querySelector("h1");
+// headerEditor.id = "victory"
+// newHeader.innerHTML = 'Alex is the champion'
+// console.log(headerEditor.nodeName);
+// console.log(headerEditor.id);
+// console.log(headerEditor.innerHTML);
